934f7b2a474fdd1499acc8e937839cef5c9a15c9
ali aygen turan <ali00aygen@hotmail.com>
2021-07-27 01:35:41 +0300

    26 temmuz gün sonu

---
+++ Koleksionlar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksionlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            Console.WriteLine("----------------------------------------------");
+            isimler = new string[5];
+            isimler[4] = "ilker";
+
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+            Console.WriteLine("---------------------------------------------------------------");
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine("----------------------------------------------");
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
